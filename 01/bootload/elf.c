35d197e77bf4ece5459ec74c3ad271d49f9c6a91
KodaiD <bb_dkoudai@yahoo.co.jp>
2021-02-24 20:48:37 +0900

    done: bootloader (step6)

--- 01/bootload/elf.c
+++ 01/bootload/elf.c
@@ -28,14 +28,14 @@ struct elf_header {
 };
 
 struct elf_program_header {
-  long type;          /* セグメントの種別 */
-  long offset;        /* ファイル中の位置 */
-  long virtual_addr;  /* 論理アドレス */
-  long physical_addr; /* 物理アドレス */
-  long file_size;     /* ファイル中のサイズ */
-  long memory_size;   /* メモリ上でのサイズ */
-  long flags;         /* 各種フラグ */
-  long align;         /* アラインメント */
+  long type;                    /* セグメントの種別 */
+  long offset;                  /* ファイル中の位置 */
+  long virtual_addr;            /* 論理アドレス */
+  long physical_addr;           /* 物理アドレス */
+  long file_size;               /* ファイル中のサイズ */
+  long memory_size;             /* メモリ上でのサイズ */
+  long flags;                   /* 各種フラグ */
+  long align;                   /* アラインメント */
 };
 
 static int elf_check(struct elf_header *header)
@@ -67,27 +67,23 @@ static int elf_load_program(struct elf_header *header)
     if (phdr->type != 1)    
       continue;
 
-    putxval(phdr->offset,        6); puts(" ");
-    putxval(phdr->virtual_addr,  8); puts(" ");
-    putxval(phdr->physical_addr, 8); puts(" ");
-    putxval(phdr->file_size,     5); puts(" ");
-    putxval(phdr->memory_size,   5); puts(" ");
-    putxval(phdr->flags,         2); puts(" ");
-    putxval(phdr->align,         2); puts("\n");
+    memcpy((char *)phdr->physical_addr, (char *)header + phdr->offset, phdr->file_size);
+    memset((char *)phdr->physical_addr + phdr->file_size, 0,
+      phdr->memory_size - phdr->file_size);
   }
 
   return 0;
 }
 
-int elf_load(char *buf)
+char elf_load(char *buf)
 {
   struct elf_header * header = (struct elf_header *)buf;
 
   if (elf_check(header) < 0)
-    return -1;
+    return NULL;
 
   if (elf_load_program(header) < 0)
-    return -1;
+    return NULL;
 
-  return 0;
+  return (char *)header->entry_point;
 }
